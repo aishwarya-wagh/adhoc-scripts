819ac7777f61872b560237f4acd2e2a7a35919c0
Aishwarya Wagh <98280525+aishwarya-wagh@users.noreply.github.com>
2025-03-14 20:44:10 -0400

    Update basic.sql

--- parity/basic.sql
+++ parity/basic.sql
@@ -10,12 +10,11 @@ SELECT table_schema, table_name, column_name, data_type, is_nullable, column_def
 FROM information_schema.columns
 WHERE table_catalog = 'PROD_DATABASE_NAME';
 
-SELECT table_schema, table_name, view_definition, is_secure, is_materialized
+SELECT table_schema, table_name, view_definition, is_secure
 FROM information_schema.views
 WHERE table_catalog = 'PROD_DATABASE_NAME';
 
-SELECT table_schema, table_name, view_definition
-FROM information_schema.views
+SELECT table_schema, table_name, 'MATERIALIZED VIEW' AS table_type
+FROM information_schema.tables
 WHERE table_catalog = 'PROD_DATABASE_NAME'
-  AND is_materialized = 'YES';
-
+  AND table_type = 'MATERIALIZED VIEW';
